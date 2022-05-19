87dcc8e096a33ff5381943b0cb01146f024844bf
chIorophyII <dab2in79@gmail.com>
2022-05-19 21:41:23 +0900

    profile controller

--- src/main/java/com/sparta/cicdtest/OssackController.java
+++ src/main/java/com/sparta/cicdtest/OssackController.java
@@ -1,11 +1,19 @@
 package com.sparta.cicdtest;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 public class OssackController {
+    private final Environment env;
+
     @Value("${logging-module.version}")
     private String version;
 
@@ -13,6 +21,20 @@ public class OssackController {
     public String version() {
         return String.format("Project Version : %s", version);
     }
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real1", "real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+        // real, real1, real2 중 하나라도 있으면 그 값 반환
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+
 
     @GetMapping("/health")
     public String checkHealth() {

3bb77442ad24e634698c7a10a3a9cc5013fc176a
chIorophyII <dab2in79@gmail.com>
2022-05-19 22:11:07 +0900

    module version

--- src/main/java/com/sparta/cicdtest/OssackController.java
+++ src/main/java/com/sparta/cicdtest/OssackController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class OssackController {
     private final Environment env;
 
-    @Value("${logging-module.version}")
+    @Value("${ossack-module.version}")
     private String version;
 
     @GetMapping("/")

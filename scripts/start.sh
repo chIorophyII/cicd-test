d4036df4715a19da536e692d2ba3023b05b8a0f8
chIorophyII <dab2in79@gmail.com>
2022-05-21 01:46:42 +0900

    찐막

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 # start.sh
 # !/bin/bash
 
-CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
 echo "> Current port of running WAS is ${CURRENT_PORT}."
